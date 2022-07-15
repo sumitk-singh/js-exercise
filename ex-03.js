c7a7249766bb077f1c5235e77cd3bfd684a32ab3
SUMIT SINGH <76554602+sumitk-singh@users.noreply.github.com>
2022-07-15 09:03:29 +0530

    ex-04

---
+++ ex-03.js
@@ -0,0 +1,9 @@
+//reverse the number element in array
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const fun = () => {
+  console.log(arr.reverse());
+};
+
+fun();
---
+++ ex-04.js
@@ -0,0 +1,9 @@
+// sort in accending order
+
+const arr = [65, 44, 12, 4];
+arr.forEach(fun);
+
+function fun(a, b) {
+  const x = b - a;
+  console.log(x);
+}
