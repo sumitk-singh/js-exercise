afc6e937abfe526db1566ca5a8cc6e6fdc3d88c5
SUMIT SINGH <76554602+sumitk-singh@users.noreply.github.com>
2022-08-06 00:40:00 +0530

    added another method of doing the same task

--- ex-09.js
+++ ex-09.js
@@ -2,5 +2,12 @@
 
 const arr = [0, 3, 5, 2, 0, 6];
 
-const result = Math.max.apply(null, arr);
-console.log(result);
+//method 1
+// const result_1 = Math.max.apply(null, arr);
+// console.log(result_1);
+
+//method 2
+// using spread operator
+
+const result_2 = Math.max(...arr);
+console.log(result_2);
