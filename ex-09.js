56ca3bdb22470ca1108c06d165480d278f495660
SUMIT SINGH <76554602+sumitk-singh@users.noreply.github.com>
2022-08-06 00:43:48 +0530

    .

--- ex-09.js
+++ ex-09.js
@@ -1,13 +1,14 @@
 // find out the max and min from the given array
 
-const arr = [0, 3, 5, 2, 0, 6];
+const arr = [0, 3, 5, 2, 0, 16];
 
 //method 1
-// const result_1 = Math.max.apply(null, arr);
-// console.log(result_1);
+// using apply
+const result_1 = Math.max.apply(null, arr);
+console.log(result_1);
 
 //method 2
 // using spread operator
 
-const result_2 = Math.max(...arr);
+const result_2 = Math.avg(...arr);
 console.log(result_2);
