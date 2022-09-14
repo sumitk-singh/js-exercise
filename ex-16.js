5299d35fe828dcf69852f931beb70f6dcae77cea
sumit-0-0-1 <sumit.email.dev@gmail.com>
2022-09-14 09:12:59 +0530

    added ex-16

---
+++ ex-16.js
@@ -0,0 +1,25 @@
+// Ques 16.1
+// Check Odd or Even
+
+// const number = prompt("enter number");
+
+// if (number % 2 == 0) {
+//     console.log(`${number} is even number`);
+// }
+// else {
+//     console.log(`${number} is odd number`);
+// }
+
+//--------------------------------------------------------------
+// Ques 16.2
+// List all the even number in between 1 to 100
+
+let count = 0;
+
+for(let i=1;i<=5;i++){
+    if(i%2==0){
+        count = count +1;
+        console.log(count);
+    }
+}
+
