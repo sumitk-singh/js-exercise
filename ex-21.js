1cb65aae387a6adee4123ed8eed1376c8517be40
sumit-0-0-1 <sumit.email.dev@gmail.com>
2022-09-16 09:00:33 +0530

    added ex-21

--- ex-21.js
+++ ex-21.js
@@ -1,1 +1,22 @@
-//Palindrome String
+//Check Palindrome Number
+
+const number = 121;
+
+if(String(number)==String(number).split("").reverse().join("")){
+    console.log(`${number} is palindrome number`);
+}
+
+else{
+    console.log(`${number} is not a palindrome number`);
+}
+
+
+
+
+
+
+
+
+
+
+
