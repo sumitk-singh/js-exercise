615df5a0a0d39cf8376b301e542b83fb1f7c5bb6
sumit-0-0-1 <sumit.email.dev@gmail.com>
2022-09-17 08:14:25 +0530

    added ex-20

--- ex-20.js
+++ ex-20.js
@@ -2,7 +2,18 @@
 //Prime Numbers upto N 
 
 const number = 25;
+let count = 0;
 
-for (let i = 0; i <= number; i++) {
+for (let i = 1; i <= number; i++) {
+    if (number % i == 0) {
+        count = count + 1;
+        break;
+    }
+}
 
+if (count == 2) {
+    console.log(`${number} is prime number`);
+}
+else {
+    console.log(`${number} is not a prime number`);
 }
