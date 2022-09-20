c07b4f355d6b145db22be1faad002848ca2c34db
sumit-0-0-1 <sumit.email.dev@gmail.com>
2022-09-20 07:51:54 +0530

    added ex22.js

--- ex-22.js
+++ ex-22.js
@@ -4,14 +4,14 @@ const x1 = 10;          //a number
 const x2 = "5";         //a string
 
 const cal = (a, b) => {
-    const sum = x1 + x2;
-    const sub = x1 - x2;
-    const prod = x1 * x2;
-    const div = x1 / x2;
+    const sum = a + b;
+    const sub = a - b;
+    const prod = a * b;
+    const div = a / b;
     console.log(sum);
     console.log(sub);
     console.log(prod);
     console.log(div);
 }
 
-cal();
+cal(x1, x2);
