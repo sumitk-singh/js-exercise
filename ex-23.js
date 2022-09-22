38e0d8711047bf9055ca67a7f165f7997b010526
sumit-0-0-1 <sumit.email.dev@gmail.com>
2022-09-22 08:03:36 +0530

    added ex-23

---
+++ ex-23.js
@@ -0,0 +1,16 @@
+//Find the most frequent item of an array
+
+const arr = [1, 2, 2, 5, 4, 4, 4, 4, 6];
+
+let arrLength = arr.length;
+let maxNumber = Math.max(...arr);
+
+console.log(arrLength);
+console.log(maxNumber);
+
+for (let i = 0; i <= arrLength; i++) {
+
+}
+
+
+
