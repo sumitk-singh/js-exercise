a3b13b0ce0d5168dc0313948b969682d7a533c10
sumit-0-0-1 <sumit.email.dev@gmail.com>
2022-09-28 11:28:55 +0530

    added ex-25

---
+++ ex-25.js
@@ -0,0 +1,15 @@
+//sort in accending and decending order
+const product = ["speaker", "shoes", "bag"];
+const price = [23, 8, 1, 2, 3, 4, 5];
+
+// in accending order
+const acc_order_product = product.sort();
+console.log(acc_order_product);
+
+const acc_order_price = price.sort(function (a, b) {
+    return (a - b);
+})
+
+console.log(acc_order_price);
+
+//in decending order
