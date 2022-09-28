e2c3a46001c2639f0d954ed03a2aacc86325d320
sumit-0-0-1 <sumit.email.dev@gmail.com>
2022-09-28 12:22:20 +0530

    added ex-26

---
+++ ex-26.js
@@ -0,0 +1,18 @@
+
+//example
+
+//1. while 
+let count_1 = 5;
+while (count_1) {
+    console.log("this is from while loop block, counted: "+ count_1);
+    count_1--;
+}
+
+//2. do while
+
+let count_2 = 10;
+let count_3 = 5;
+do {
+    console.log("this is form do while loop block, counted: " + count_2);
+    count_2--;
+} while (count_3 <= count_2);
