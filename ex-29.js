40073abc35ed67c25019ab889d9c72e803c5461e
SUMIT SINGH <76554602+sumitk-singh@users.noreply.github.com>
2023-01-24 01:24:03 +0530

    Update ex-29.js

--- ex-29.js
+++ ex-29.js
@@ -1,5 +1,11 @@
-var numbers = [4, 2, 5, 1, 3];
-numbers.sort(function(a, b) {
-  return a - b;
-});
-console.log(numbers);
+const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape"];
+
+function getRandomFruit() {
+  // Generate a random index between 0 and the last index of the fruits array
+  const randomIndex = Math.floor(Math.random() * fruits.length);
+  
+  // Return the fruit name at the randomly generated index
+  return fruits[randomIndex];
+}
+
+console.log(getRandomFruit()); // Example output: "Banana"
